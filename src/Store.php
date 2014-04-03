fa4d32c73fd8bd6324fa988da8bd3e8fd0b3527d
Ekow Abaka Ainooson <jainooson@gmail.com>
2014-04-03 20:17:19 +0000

    Stores now throws an error when no store can be found

--- src/Store.php
+++ src/Store.php
@@ -10,6 +10,11 @@ abstract class Store
     
     public static function factory($params)
     {
+        if(!isset($params['store']) || $params['store'] == '')
+        {
+            fputs(STDERR, "Please specify a store type using the --store option\n");
+            die();
+        }
         $storeDriverClass = '\\ajumamoro\\stores\\' . ucfirst($params['store']) . 'Store';
         $storeDriver = new $storeDriverClass($params);
         $storeDriver->init();
