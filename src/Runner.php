6168b4d4545039b0a81c8550b87c6fdfca9e1db5
James Ainooson <jainooson@gmail.com>
2017-06-07 06:10:18 -0500

    Config made singleton for runner

--- src/Runner.php
+++ src/Runner.php
@@ -72,7 +72,8 @@ class Runner
     }
 
     public function mainLoop() {
-        $bootstrap = $this->config->get('bootstrap');
+        $bootstrap = $this->config->get('broker');
+        var_dump($bootstrap);
         if ($bootstrap) {
             require $this->config->get('bootstrap');
         }
