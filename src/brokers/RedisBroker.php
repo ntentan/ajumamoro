a8fdf806fb078adcec8a55e92a75439ee9bee548
James Ainooson <jainooson@gmail.com>
2017-11-26 15:42:02 -0600

    Switched config format for redis broker

--- src/brokers/RedisBroker.php
+++ src/brokers/RedisBroker.php
@@ -4,6 +4,8 @@ namespace ajumamoro\brokers;
 
 use ajumamoro\BrokerInterface;
 use ajumamoro\exceptions\BrokerConnectionException;
+use Predis\Client;
+use Predis\CommunicationException;
 
 /**
  * Description of RedisBroker
@@ -15,15 +17,16 @@ class RedisBroker implements BrokerInterface
 
     /**
      * Instance of the redis client
-     * @var \Predis\Client
+     * @var Client
      */
     private $redis;
 
-    public function __construct($config) {
-        $this->redis = new \Predis\Client($config);
+    public function __construct($config)
+    {
+        $this->redis = new Client($config['parameters'], $config['options'] ?? null);
         try {
             $this->redis->connect();
-        } catch (\Predis\CommunicationException $ex) {
+        } catch (CommunicationException $ex) {
             throw new BrokerConnectionException(
                 "Failed to connect to redis broker: {$ex->getMessage()}"
             );
@@ -31,10 +34,11 @@ class RedisBroker implements BrokerInterface
     }
 
     /**
-     * 
+     *
      * @return Job
      */
-    public function get() {
+    public function get()
+    {
         do {
             $response = $this->redis->rpop("job_queue");
             usleep(500);
@@ -42,7 +46,8 @@ class RedisBroker implements BrokerInterface
         return unserialize($response);
     }
 
-    public function put($job) {
+    public function put($job)
+    {
         $job['id'] = $this->redis->incr("job_id_sequence");
         $this->redis->lpush("job_queue", serialize($job));
         return $job['id'];
@@ -53,11 +58,13 @@ class RedisBroker implements BrokerInterface
      * @param $jobId
      * @return string
      */
-    public function getStatus($jobId) {
+    public function getStatus($jobId)
+    {
         return json_decode($this->redis->get("job_status:$jobId"), true);
     }
 
-    public function setStatus($jobId, $status) {
+    public function setStatus($jobId, $status)
+    {
         return $this->redis->set("job_status:$jobId", json_encode($status));
     }
 
